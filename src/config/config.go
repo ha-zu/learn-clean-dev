8f25bcfd1f0d3796107123b7464e9ad0a9e558cd
ha-zu <nagaoka0132@gmail.com>
2022-12-01 15:06:00 +0900

    Deleted unnecessary files.

--- src/config/config.go
+++ src/config/config.go
@@ -5,14 +5,13 @@ import (
 )
 
 type Config struct {
-	DevURL  string `envconfig:"DEV_URL" default:"http:localhost:8080"`
-	DevPort string `envconfig:"DEV_PORT" default:"8080"`
+	DevURL  string `envconfig:"DEV_URL" default:"http:localhost:1323"`
+	DevPort string `envconfig:"DEV_PORT" default:"1323"`
 }
 
 var Env Config
 
 func init() {
-	EnvSetting()
 	err := envconfig.Process("", &Env)
 	if err != nil {
 		// ToDo: Error Handling
