912437f850716eada2880524247a0971e1f0eaf9
ha-zu <nagaoka0132@gmail.com>
2023-01-11 02:11:11 +0900

    fix category name

--- src/entity/category/category_name.go
+++ src/entity/category/category_name.go
@@ -4,12 +4,12 @@ import custerr "github.com/ha-zu/learn-clean-dev/src/entity/customerror"
 
 type CategoryName string
 
-func NewCategoryName(name CategoryName) error {
+func NewCategoryName(name string) (CategoryName, error) {
 
 	if name == "" {
-		return custerr.ErrEmptyValue
+		return CategoryName(""), custerr.ErrEmptyValue
 	}
 
-	return nil
+	return CategoryName(name), nil
 
 }
